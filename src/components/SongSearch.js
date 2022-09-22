bac6b8c47143b85aa19f9cf68983fb7a0bf2f88d
ROBERT-Gimenez <robertgim37@gmail.com>
2022-09-22 00:00:42 -0300

    adding asynchronous apis request

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect } from 'react';
+import { helpHttp } from './helpers/helpHttp';
 import Loader from './Loader';
 import SongDetail from './SongDetail';
 import SongForm from './SongForm';
@@ -9,8 +10,31 @@ const SongSearch = () => {
     const [bio,setBio] = useState(null);
     const [loading,setLoading] = useState(false);
 
+    useEffect(() => {
+        if(search === null){return};
+
+        const fetchData = async() => {
+            const {artist,song} = search;
+
+            let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+            let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+            setLoading(true);
+
+            const [artistRes,songRest] = await Promise.all([
+                helpHttp().get(artistUrl),
+                helpHttp().get(songUrl),
+            ]);
+
+            setBio(artistRes);
+            setLyric(songRest);
+            setLoading(false);
+        };
+        fetchData();
+    },[search])
+
     const handleSearch = (data) => {
-        console.log(data);
+        //console.log(data);
         setSearch(data);
     }
 
